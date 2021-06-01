03245cb2ff78d09f74ed84052a0283748417a1cc
Alihan Akyol <56774072+akyola@users.noreply.github.com>
2021-06-01 16:45:16 +0300

    Add files via upload

---
+++ Firmware/Debug/Core/Startup/startup_stm32f205vgtx.d
@@ -0,0 +1,2 @@
+Core/Startup/startup_stm32f205vgtx.o: \
+ ../Core/Startup/startup_stm32f205vgtx.s
---
+++ Firmware/Debug/Core/Startup/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32f205vgtx.s 
+
+OBJS += \
+./Core/Startup/startup_stm32f205vgtx.o 
+
+S_DEPS += \
+./Core/Startup/startup_stm32f205vgtx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/startup_stm32f205vgtx.o: ../Core/Startup/startup_stm32f205vgtx.s
+	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Core/Startup/startup_stm32f205vgtx.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+
